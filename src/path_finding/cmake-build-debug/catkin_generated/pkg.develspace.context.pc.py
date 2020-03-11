5e9606dcdd98b6e921f0ed3eb61ae318b942666f
linyicheng1 <hust_linyicheng@qq.com>
2020-03-11 21:13:23 +0800

    rm debug file

--- src/path_finding/cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/lyc/motionPlan/src/path_finding/include".split(';') if "/home/lyc/motionPlan/src/path_finding/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "path_finding"
-PROJECT_SPACE_DIR = "/home/lyc/motionPlan/src/path_finding/cmake-build-debug/devel"
-PROJECT_VERSION = "0.0.0"
